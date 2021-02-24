1b44ab27daaed0807db89b943da3ebe0f8a477f3
Abel Lavieri <abel.lavieri@gmail.com>
2021-02-23 22:26:11 -0400

    Add follow and unfollow methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,16 @@ class UsersController < ApplicationController
   def index
     @user = current_user
   end
+
+  def follow
+    @user = User.find(params[:id])
+    current_user.followees << @user
+    redirect_back(fallback_location: home_path)
+  end
+
+  def unfollow
+    @user = User.find(params[:id])
+    current_user.followed_users.find_by(followed_id: @user.id).destroy
+    redirect_back(fallback_location: home_path)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,13 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     sessions: 'users/sessions'
   }
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
+  post 'users/:id/follow', to: 'users#follow', as: 'follow_user'
+  post 'users/:id/unfollow', to: 'users#unfollow', as: 'unfollow_user'
   resources :users, only: %i[index show]
+  resources :posts do
+    resources :comments
+  end
 
   root to: 'users#index'
 end
