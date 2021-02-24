9697147fa85a1d9a4769292f7da54b61b9d0b144
Abel Lavieri <abel.lavieri@gmail.com>
2021-02-23 22:09:26 -0400

    Add routes to comments and posts

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,10 @@ class User < ApplicationRecord
 
   has_many :posts, foreign_key: 'AuthorId'
   has_many :comments, foreign_key: 'AuthorId'
+
+  has_many :followed_users, class_name: 'Relationship', foreign_key: 'follower_id', dependent: :destroy
+  has_many :following_users, class_name: 'Relationship', foreign_key: 'followed_id', dependent: :destroy
+
+  has_many :followees, through: :followed_users, source: :followed
+  has_many :followers, through: :following_users, source: :follower
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,39 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_20_050450) do
+ActiveRecord::Schema.define(version: 2021_02_24_012440) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.string "text"
+    t.integer "AuthorId"
+    t.string "commentable_type"
+    t.bigint "commentable_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["AuthorId"], name: "index_comments_on_AuthorId"
+    t.index ["commentable_type", "commentable_id"], name: "index_comments_on_commentable"
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "text"
+    t.integer "AuthorId"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["AuthorId"], name: "index_posts_on_AuthorId"
+  end
+
+  create_table "relationships", force: :cascade do |t|
+    t.integer "follower_id"
+    t.integer "followed_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["followed_id"], name: "index_relationships_on_followed_id"
+    t.index ["follower_id", "followed_id"], name: "index_relationships_on_follower_id_and_followed_id", unique: true
+    t.index ["follower_id"], name: "index_relationships_on_follower_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username", null: false
