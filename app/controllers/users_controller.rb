5279a754d40fbaefbb2b11882f07bf5514a2dd07
Abel Lavieri <abel.lavieri@gmail.com>
2021-02-23 23:25:03 -0400

    Update index method in user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,18 +4,38 @@ class UsersController < ApplicationController
   end
 
   def index
+    @post = Post.new
+    @comment = Comment.new
+    timeline_posts
+    who_to_follow
     @user = current_user
   end
 
   def follow
     @user = User.find(params[:id])
     current_user.followees << @user
-    redirect_back(fallback_location: home_path)
+    redirect_back(fallback_location: root_path)
   end
 
   def unfollow
     @user = User.find(params[:id])
     current_user.followed_users.find_by(followed_id: @user.id).destroy
-    redirect_back(fallback_location: home_path)
+    redirect_back(fallback_location: root_path)
+  end
+
+  private
+
+  def who_to_follow
+    following_array = current_user.followees.map { |friend| friend }
+    @user_timeline = User.where.not(id: following_array << current_user).all
+  end
+
+  def timeline_posts
+    following_array = current_user.followees.map { |friend| friend }
+    @timeline_posts ||= Post
+      .where(author: following_array.compact << current_user)
+      .all
+      .ordered_by_most_recent
+      .includes(:author)
   end
 end
