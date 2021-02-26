95a08c93b7968150012a2f52f62e6586bf813afa
Abel Lavieri <abel.lavieri@gmail.com>
2021-02-25 23:25:49 -0400

    Add method for making a discussion in a group

--- app/controllers/discussions_controller.rb
+++ app/controllers/discussions_controller.rb
@@ -1,2 +1,25 @@
 class DiscussionsController < ApplicationController
+  def new
+    @discussion = Post.new
+  end
+
+  def create
+    @discussion = Discussion.new(discussion_params)
+    current_group = Group.find(@discussion.group_id)
+    @discussion.AuthorId = current_user.id
+    @discussion.group_id = current_group.id
+    if @discussion.save
+      current_group.discussions << @discussion
+      current_user.discussions << @discussion
+      redirect_back(fallback_location: group_path(current_group.id))
+    else
+      redirect_back(fallback_location: group_path(current_group.id)), notice: 'Post could not be created!'
+    end
+  end
+
+  private
+
+  def discussion_params
+    params.require(:discussion).permit(:text, :AuthorId, :group_id)
+  end
 end
