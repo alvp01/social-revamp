b82d397855f273aa8015cb35f9793916a5c3555f
Abel Lavieri <abel.lavieri@gmail.com>
2021-02-28 01:30:16 -0400

    Add helper en partial for comment deletion in discussions

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -1,2 +1,5 @@
 module CommentsHelper
+  def delete_discussion_comment(discussion, comment)
+    render './components/delete_discussion_comment_button', discussion: discussion, comment: comment if current_user == comment.author
+  end
 end
